483d42255a6e7417c4399de3a54f3226e43cabad
cabeywickra <zcabcab@ucl.ac.uk>
2017-11-02 11:20:01 +0000

    Add databases through install.php file

--- install.php
+++ install.php
@@ -1,19 +1,32 @@
 <?php
 include './config.php';
 
-SQL::connect();
-$result = SQL::query('SHOW TABLES LIKE "entries"');
+$result1 = mysqli_query($link,'SHOW TABLES LIKE "entries"');
 
-if (count($result))
+if (count($result1))
 {
-    print '<h1>Installed! You should delete this file now.</h1> <a href="./register.php">Go to Register Page to create an acount</a>';
+    print '<h1>Installed Entries Database!</h1> ';
 }
 else
 {
     $entries = "CREATE TABLE `entries` (`id` INT(11) AUTO_INCREMENT PRIMARY KEY,`date` DATE,`text` TEXT)";
+    mysqli_query($link,$entries);
+
+    header('location: ./install.php');
+}
+
+$result2 = mysqli_query($link,'SHOW TABLES LIKE "users"');
+
+if (count($result2))
+{
+    print '<h1>Installed User Database! </h1>';
+    print '<h1>You should delete this file now.</h1> <a href="./register.php">Go to Register Page to create an acount</a>';
+}
+else
+{
     $users = "CREATE TABLE `users` (`id` INT(11) AUTO_INCREMENT PRIMARY KEY,`username` VARCHAR(50),`password` VARCHAR(255),`created_at` DATETIME CURRENT_TIMESTAMP)";
-    SQL::query($entries);
-    SQL::query($users);
+    mysqli_query($link,$users);
+
     header('location: ./install.php');
 }
 
