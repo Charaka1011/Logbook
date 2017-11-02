299aeb92d0ca0fb3c374f9356b798cc6bf267c79
haziqzharfan <haziq.zharfan.32@gmail.com>
2017-11-02 12:05:45 +0000

    Install file working
    Install.php now finally works correctly to set up Entries and User tables

--- install.php
+++ install.php
@@ -1,23 +1,20 @@
 <?php
 include './config.php';
 
-$result1 = mysqli_query($link,'SHOW TABLES LIKE "entries"');
-
-if (count($result1))
+if (mysqli_query($link,"DESCRIBE `entries`"))
 {
     print '<h1>Installed Entries Database!</h1> ';
 }
 else
 {
-    $entries = "CREATE TABLE `entries` (`id` INT(11) AUTO_INCREMENT PRIMARY KEY,`date` DATE,`text` TEXT)";
+    $entries = "CREATE TABLE `entries` (`id` INT(11) AUTO_INCREMENT PRIMARY KEY,`username` VARCHAR(50),`date` DATE,`text` TEXT)";
     mysqli_query($link,$entries);
 
     header('location: ./install.php');
 }
 
-$result2 = mysqli_query($link,'SHOW TABLES LIKE "users"');
 
-if (count($result2))
+if (mysqli_query($link,"DESCRIBE `users`"))
 {
     print '<h1>Installed User Database! </h1>';
     print '<h1>You should delete this file now.</h1> <a href="./register.php">Go to Register Page to create an acount</a>';
