832759b7dccb1a91e648ba0d069368c685cf5596
cabeywickra <zcabcab@ucl.ac.uk>
2017-11-02 14:52:47 +0000

    install file

--- config.php
+++ config.php
@@ -2,9 +2,17 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 
+define('DB_SERVER', 'team21log.mysql.database.azure.com');
+define('DB_USERNAME', 'Charaka@team21log');
+define('DB_PASSWORD', '2017Team21');
+define('DB_NAME', 'logbook');
 
 /* Attempt to connect to MySQL database */
-$link=mysqli_init(); mysqli_ssl_set($link, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($link, "team21log.mysql.database.azure.com", "Charaka@team21log", {"2017Team21"}, {"logbook"}, 3306);
+// $link=mysqli_init(); 
+// mysqli_ssl_set($link, NULL, NULL, {ca-cert filename}, NULL, NULL);
+// mysqli_real_connect($link, DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, 3306);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
 
 // Check connection
 if($link === false){
